d5bea3ed9485f57ac49de8e247b9b54c3e2bce46
trikitrok <trikitrok@gmail.com>
2015-08-30 07:04:09 +0200

    Extracted some helpers to separate responsibilities

--- src/poker_hands/core.clj
+++ src/poker_hands/core.clj
@@ -1,12 +1,27 @@
 (ns poker-hands.core)
 
-(def face-values [\1 \2 \3 \4 \5 \6 \7 \8 \9 \J \K \Q])
+(defn- compute-value [card-description]
+  (let [face-values [\1 \2 \3 \4 \5 \6 \7 \8 \9 \J \K \Q]]
+    (.indexOf face-values (first card-description))))
+
+(defn- split-in-card-descriptions [hand-description]
+  (clojure.string/split hand-description #" "))
+
+(defn- card [card-description]
+  {:face (str (first card-description))
+  :value (compute-value card-description)
+  :suit (str (second card-description))})
+
+(def ^:private create-cards (partial map card))
+
+(defn highest-card [hand]
+  (apply max-key :value hand))
 
 (defn hand [hand-description]
-  (map #(hash-map :face (str (first %))
-                  :value (.indexOf face-values (first %))
-                  :suit (str (second %)))
-       (clojure.string/split hand-description #" ")))
+  (-> hand-description
+      split-in-card-descriptions
+      create-cards))
 
 (defn score [hand]
-  (str "high card: " (:face (apply max-key :value hand))))
+  (str "high card: "
+       (:face (highest-card hand))))
