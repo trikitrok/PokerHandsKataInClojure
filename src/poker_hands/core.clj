00c82d0db8694ba12c8780dea270f2886f2f2135
trikitrok <trikitrok@gmail.com>
2015-08-30 06:52:52 +0200

    Scoring high card

--- src/poker_hands/core.clj
+++ src/poker_hands/core.clj
@@ -1,7 +1,12 @@
 (ns poker-hands.core)
 
+(def face-values [\1 \2 \3 \4 \5 \6 \7 \8 \9 \J \K \Q])
+
 (defn hand [hand-description]
-  )
+  (map #(hash-map :face (str (first %))
+                  :value (.indexOf face-values (first %))
+                  :suit (str (second %)))
+       (clojure.string/split hand-description #" ")))
 
 (defn score [hand]
-  "high card: 9")
+  (str "high card: " (:face (apply max-key :value hand))))
--- test/poker_hands/core_test.clj
+++ test/poker_hands/core_test.clj
@@ -8,4 +8,5 @@
   (fact
     "it scores a High card hand"
 
-    (score (hand "2H 3D 5S 9C KD")) => "high card: 9"))
+    (score (hand "2H 3D 5S 9C 2D")) => "high card: 9"
+    (score (hand "2H 8D 5S 9C KD")) => "high card: K"))
