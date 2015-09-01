dd4f0f0c0f1de9ca16bfa87771e0f84880b6e178
trikitrok <trikitrok@gmail.com>
2015-09-01 17:06:39 +0200

    Extracted some helpers+

--- src/poker_hands/ranking_system.clj
+++ src/poker_hands/ranking_system.clj
@@ -48,30 +48,39 @@
   (let [hands-ranking [HighCard Pair TwoPairs Triplet Straight Flush FullHouse FourKind StraightFlush]]
     (.indexOf hands-ranking (class hand))))
 
-(defn- result [hand]
+(defn- compute-first-different-pair-of-values [hand1 hand2]
+  (map compute-value
+       (first
+         (drop-while #(= (first %) (second %))
+                     (map vector
+                          (:cards hand1)
+                          (:cards hand2))))))
+
+(defn- untie [hand1 hand2]
+  (let
+    [first-different-pair-of-values (compute-first-different-pair-of-values hand1 hand2)]
+    (if (empty? first-different-pair-of-values)
+      nil
+      (if (> (first first-different-pair-of-values)
+             (second first-different-pair-of-values))
+        hand1
+        hand2))))
+
+(defn- wrap-in-result [hand]
   (if (nil? hand)
     {:winner :no-winner}
     {:winner  (:player hand)
      :message (victory-message hand)}))
 
-(defn- untie [hand1 hand2]
-  (let
-    [first-different-values
-     (map compute-value
-          (first
-            (drop-while #(= (first %) (second %))
-                        (map vector
-                             (:cards hand1)
-                             (:cards hand2)))))]
-    (if (empty? first-different-values)
-      nil
-      (if (> (first first-different-values) (second first-different-values))
-        hand1
-        hand2))))
+(defn- compute-winner-hand [[hand1 hand2]]
+  (let [ranking1 (ranking hand1)
+        ranking2 (ranking hand2)]
+    (cond
+      (> ranking1 ranking2) hand1
+      (< ranking1 ranking2) hand2
+      :else (untie hand1 hand2))))
 
 (defn compare-hands [hand1 hand2]
-  (result
-    (cond
-      (> (ranking hand1) (ranking hand2)) hand1
-      (< (ranking hand1) (ranking hand2)) hand2
-      :else (untie hand1 hand2))))
+  (-> [hand1 hand2]
+      compute-winner-hand
+      wrap-in-result))
