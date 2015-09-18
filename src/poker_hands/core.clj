4be3f376dd51bc6b486485233497b1397293089f
trikitrok <trikitrok@gmail.com>
2015-09-18 16:20:13 +0200

    Renamed some functions

--- src/poker_hands/core.clj
+++ src/poker_hands/core.clj
@@ -4,29 +4,27 @@
   (:require [poker-hands.results :as results]))
 
 (defn extract-player [player-hand]
-  (let [player-description (first (clojure.string/split player-hand #":"))]
-    (if (= player-description "Black")
-      :black
-      :white)))
+  (let [player (first (clojure.string/split player-hand #":"))]
+    (if (= player "Black") :black :white)))
 
 (defn extract-hand-description [player-hand]
   (clojure.string/trim (second (clojure.string/split player-hand #":"))))
 
-(defn- create-players-hands [player-hands]
+(defn- create-hands [player-hands]
   (let [players (map extract-player player-hands)
         hand-descriptions (map extract-hand-description player-hands)]
     (map #(assoc (hands/create %1) :player %2)
          hand-descriptions
          players)))
 
-(defn- compare-player-hands [[hand1 hand2]]
+(defn- compare-hands [[hand1 hand2]]
   (ranking/compare-hands hand1 hand2))
 
-(defn- show-result-as-string [winning-hand]
+(defn- show-result [winning-hand]
   (results/message winning-hand))
 
-(defn compare-hands [player1-hand player2-hand]
-  (-> [player1-hand player2-hand]
-      create-players-hands
-      compare-player-hands
-      show-result-as-string))
+(defn compare-players-hands [& hands-descriptions]
+  (-> hands-descriptions
+      create-hands
+      compare-hands
+      show-result))
--- test/poker_hands/core_test.clj
+++ test/poker_hands/core_test.clj
@@ -5,10 +5,10 @@
 (facts
   "About comparing poker hands"
 
-  (compare-hands
+  (compare-players-hands
     "Black: 5H 7D JS 9C KD"
     "White: 5S 5D 10H 10S 8C") => "White wins. - with Two Pairs: 10 and 5"
 
-  (compare-hands
+  (compare-players-hands
     "White: 5H 7D JS 9C KD"
     "Black: KC JC 9C 3C 2C") => "Black wins. - with a Flush of K J 9 3 2")
