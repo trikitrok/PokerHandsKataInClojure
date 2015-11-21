7bd1532021e43251b5235c604bea28e24e676260
trikitrok <trikitrok@gmail.com>
2015-11-21 20:37:41 +0100

    Renamed factories

--- src/poker_hands/creation.clj
+++ src/poker_hands/creation.clj
@@ -2,52 +2,52 @@
   (:require [poker-hands.cards :as cards])
   (:require [poker-hands.hand-types :as hand-types]))
 
-(defn- a-high-card [cards]
+(defn- a-high-card-hand [cards]
   (hand-types/->HighCard
     (cards/sorted-faces cards)))
 
-(defn- a-flush [cards]
+(defn- a-flush-hand [cards]
   (hand-types/->Flush
     (cards/sorted-faces cards)))
 
-(defn- a-pair [cards]
+(defn- a-pair-hand [cards]
   (hand-types/->Pair
     (cards/concat-cards-of-groups-with-and-without 2 cards)))
 
-(defn- a-triplet [cards]
+(defn- a-triplet-hand [cards]
   (hand-types/->Triplet
     (cards/concat-cards-of-groups-with-and-without 3 cards)))
 
-(defn- a-two-pairs [cards]
+(defn- a-two-pairs-hand [cards]
   (hand-types/->TwoPairs
     (cards/concat-cards-of-groups-with-and-without 2 cards)))
 
-(defn- a-straight [cards]
+(defn- a-straight-hand [cards]
   (hand-types/->Straight
     [(cards/straight-highest-card-face cards)]))
 
-(defn- a-straight-flush [cards]
+(defn- a-straight-flush-hand [cards]
   (hand-types/->StraightFlush
     [(cards/straight-highest-card-face cards)]))
 
-(defn- a-full-house [cards]
+(defn- a-full-house-hand [cards]
   (hand-types/->FullHouse
     (cards/concat-cards-of-groups-with-and-without 3 cards)))
 
-(defn- a-four-kind [cards]
+(defn- a-four-kind-hand [cards]
   (hand-types/->FourKind
     (cards/concat-cards-of-groups-with-and-without 4 cards)))
 
 (def ^:private factories-by-type
-  {:high-card      a-high-card
-   :flush          a-flush
-   :pair           a-pair
-   :triplet        a-triplet
-   :two-pairs      a-two-pairs
-   :straight       a-straight
-   :straight-flush a-straight-flush
-   :full-house     a-full-house
-   :four-kind      a-four-kind})
+  {:high-card      a-high-card-hand
+   :flush          a-flush-hand
+   :pair           a-pair-hand
+   :triplet        a-triplet-hand
+   :two-pairs      a-two-pairs-hand
+   :straight       a-straight-hand
+   :straight-flush a-straight-flush-hand
+   :full-house     a-full-house-hand
+   :four-kind      a-four-kind-hand})
 
 (defn create-hand [{hand-type :type cards :cards}]
   (let [factory (get factories-by-type hand-type)]
